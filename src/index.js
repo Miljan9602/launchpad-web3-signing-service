c6a8ac8b682922b65893a3e4d52ba15fe8c175c6
Miljan9602 <rakitamiljan@yahoo.com>
2021-07-01 16:48:20 +0200

    Added test code for the sign-order

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ const express = require('express')
 const app = express()
 app.use(express.json());
 const ethSig = require('eth-sig-util');
+const web3 = require("web3");
 
 app.post('/recover-typed-signature', (request, response) => {
 
     const address = request.body.address
     const data = request.body.data
     const signature = request.body.signature
-
 
     const recovered = ethSig.recoverTypedSignature({
         data: [data],
@@ -26,6 +26,29 @@ app.post('/recover-typed-signature', (request, response) => {
         "expected_signer" : address.toLowerCase(),
         "verification_status": verificationStatus
     });
+});
+
+
+app.post('/sign-order', (request, response) => {
+
+    // let user_address = request.body.user_address
+    // let roundId = request.body.roundId
+    // let address = request.body.address
+    //
+    // let hash = "0x" + ethereumjs.ABI.soliditySHA3(
+    //     ["address", "uint256", "address"],
+    //     [user_address, roundId, sale_contract_address]
+    // ).toString("hex");
+    //
+    // let result = web3.personal.sign(hash, web3.eth.defaultAccount, callback);
+    //
+    // return response.json({
+    //     "result" : result
+    // })
+
+    return response.json({
+        "result" : "test"
+    })
 });
 
 app.listen(process.env.PORT || 3000 , () => {
