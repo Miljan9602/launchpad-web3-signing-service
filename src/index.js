f7e7ea22df6c17dd73f8bb0892516fa5302cbd86
madjarevicn <nikola.madjarevic@gmail.com>
2021-07-04 13:51:53 +0200

    Add signatures.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 app.use(express.json());
 const ethSig = require('eth-sig-util');
-const web3 = require("web3");
+const Web3 = require("web3");
 
 app.post('/recover-typed-signature', (request, response) => {
 
@@ -33,10 +33,18 @@ app.post('/sign-registration', (request, response) => {
 
     let user_address = request.body.user_address
     let roundId = request.body.round_id
-    let contractAddress = request.body.address
+    let contractAddress = request.body.contract_address
+
+    const pk = "905cfc35fa3ba0b42a5293306ccc74b4bdf6a0583ed2d3117ef436e9be6715ac";
+
+
+    const web3 = new Web3(new Web3.providers.HttpProvider('https://api.avax.network/ext/bc/C/rpc'));
+
+    const account = web3.eth.accounts.privateKeyToAccount(pk);
 
     let hash = web3.utils.soliditySha3({t:"address", v: user_address}, {t: "uint256", v: roundId}, {t: "address", v: contractAddress});
-    let result = web3.eth.personal.sign(hash, web3.eth.defaultAccount, callback);
+
+    let result = web3.eth.accounts.sign(hash, pk);
 
     return response.json({
         "result" : result
