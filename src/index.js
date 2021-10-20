c61cc7d4051a3c28db15daba7b4ff2f585595817
Miljan9602 <rakitamiljan@yahoo.com>
2021-10-20 18:39:12 +0200

    Added check for the contract_address

--- src/index.js
+++ src/index.js
@@ -390,10 +390,17 @@ app.post('/airdrop/get-signature', async (request, response) => {
     let private_key = process.env.PRIVATE_KEY_1;
     let address = request.body.address
     let amountWei = request.body.amount
+    let contract_address = request.body.contract_address;
 
     const web3 = new Web3(new Web3.providers.HttpProvider(AVALAUNCH_URL));
 
-    let hash = web3.utils.soliditySha3({t:"address", v: address}, {t: "uint256", v: amountWei});
+    /**
+     * If the contract_address is address from the HCT sale. We are using the old hash version.
+     * @type {string}
+     */
+    let hash = contract_address.toUpperCase() === '0X87B1BEAAB6F7A40B28A2DEF896C50E57061C3B36' ?
+        web3.utils.soliditySha3({t:"address", v: address}, {t: "uint256", v: amountWei}) :
+        web3.utils.soliditySha3({t:"address", v: address}, {t: "uint256", v: amountWei}, {t: "address", v: contract_address});
 
     let result = web3.eth.accounts.sign(hash, private_key);
 
