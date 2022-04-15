a767222519741640c8685cda9c904cba442432bc
Milan Mitic <milanmitic@Milans-MacBook-Pro.local>
2022-04-15 14:04:32 +0200

    Configured middleware

--- src/index.js
+++ src/index.js
@@ -4,19 +4,19 @@ app.use(express.json());
 
 // require('dotenv').config()
 
+const middleware = require('./middleware/middleware')
 const air_drop_router = require('./routes/airdrop_routes')
 const sale_router = require('./routes/sale_routes')
 const utils_router = require('./routes/utils_routes')
 const collateral_router = require('./routes/collateral_routes')
 const transaction_router = require('./routes/transaction_routes')
-const middleware = require('./middleware/middleware')
 
+app.use(middleware);
 app.use('/airdrop', air_drop_router);
 app.use('/sale', sale_router);
 app.use('/utils', utils_router);
 app.use('/collateral', collateral_router);
 app.use('/transaction', transaction_router);
-app.use(middleware);
 
 app.listen(process.env.PORT || 3000 , () => {
     console.log(`🚀  Running on the ${3000 || process.env.PORT} port.`);
