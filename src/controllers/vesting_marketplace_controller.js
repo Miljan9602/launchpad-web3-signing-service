aeb08316b654262f29b8d7e5a9a4beee71afb1f7
Miljan9602 <rakitamiljan@yahoo.com>
2022-09-12 23:50:56 +0200

    added better error handling

--- src/controllers/vesting_marketplace_controller.js
+++ src/controllers/vesting_marketplace_controller.js
@@ -1,6 +1,7 @@
 const Web3 = require("web3");
 const LogDecoder = require('logs-decoder')
 const contractGetters = require("../utils/getters")
+const {Buffer} = require("avalanche");
 
 contractGetters.getCollateralContract()
 
@@ -11,24 +12,16 @@ logsDecoder.addABI(contractGetters.getMarketplaceContractAbi())
 
 exports.decode_portion_listed = async (request, response) => {
 
-    // Get hash from a body.
-    let txHash = request.body.tx_hash
+    let logs = parseTransactionLogs(request.body.tx_hash)
 
-    // Get the receipt.
-    let web3 = new Web3(new Web3.providers.HttpProvider(AVALAUNCH_URL));
-    let receipt = await web3.eth.getTransactionReceipt(txHash);
-
-    // decode logs.
-    let decoded = logsDecoder.decodeLogs(receipt.logs);
-
-    let logs = [];
-
-    for (let i = 0; i < decoded.length; i++) {
-        logs.push({
-            'name': decoded[i].name,
-            'block_number' : decoded[i].blockNumber,
-            'transaction_hash' : decoded[i].transactionHash,
-            'events' : decoded[i].events
+    if (logs === null) {
+        return response.status(400).json({
+            'status' : 'fail',
+            'error' : {
+                'message' : 'Invalid transaction. Please check that transaction is valid, and it belongs to right event.',
+                'code' : 400,
+                'type' : 'invalid_transaction'
+            }
         })
     }
 
@@ -36,4 +29,36 @@ exports.decode_portion_listed = async (request, response) => {
         'status' : 'ok',
         'events' : logs
     });
+}
+
+async function parseTransactionLogs(txHash) {
+
+    // Get the receipt.
+    let web3 = new Web3(new Web3.providers.HttpProvider(AVALAUNCH_URL));
+    let receipt = await web3.eth.getTransactionReceipt(txHash);
+
+    if (receipt.status !== true) {
+        return null;
+    }
+
+    // decode logs.
+    let decoded = logsDecoder.decodeLogs(receipt.logs);
+
+    let logs = [];
+
+    for (let i = 0; i < decoded.length; i++) {
+
+        if ( decoded[i] === undefined) {
+            return null;
+        }
+
+        logs.push({
+            'name': decoded[i].name,
+            'block_number': decoded[i].blockNumber,
+            'transaction_hash': decoded[i].transactionHash,
+            'events': decoded[i].events
+        })
+    }
+
+    return logs
 }
