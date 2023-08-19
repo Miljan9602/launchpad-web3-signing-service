1031f3b7c5a81ff6bde4ffdac287522ef3b86046
Miljan9602 <rakitamiljan@yahoo.com>
2023-08-19 13:05:41 +0200

    Implemented all signings

--- src/controllers/signing_controller.js
+++ src/controllers/signing_controller.js
@@ -6,153 +6,136 @@ const contractGetters = require("../utils/getters")
 const RPC_URL = contractGetters.getRpc()
 
 exports.sign_claim_application_token = (request, response) => {
-    const address = request.body.address
-    const data = request.body.data
-    const signature = request.body.signature
+    let amount = request.body.amount;
+    let fee = request.body.fee;
+    let sigExpTime = request.body.signature_expiration_time
 
-    const recovered = ethSig.recoverTypedSignature_v4({
-        data: data,
-        sig: signature,
-    });
+    const pk = process.env.PRIVATE_KEY_1;
+    const web3 = new Web3(new Web3.providers.HttpProvider(RPC_URL));
 
-    let verificationStatus = recovered.toLowerCase() === address.toString().toLowerCase()
+    let hash = web3.utils.soliditySha3({t:"uint256", v:amount},{t:"uint256", v:fee},{t:"uint256", v:sigExpTime}, {t: "string", v: "claimArbr"});
 
     return response.json({
-        "message_signer" : recovered.toLowerCase(),
-        "expected_signer" : address.toLowerCase(),
-        "verification_status": verificationStatus
+        "amount" : amount,
+        "fee" : fee,
+        "sigExpTime": sigExpTime,
+        "signature" : web3.eth.accounts.sign(hash, pk)
     });
 }
 
 exports.sign_claim_mint_token = (request, response) => {
-    const address = request.body.address
-    const data = request.body.data
-    const signature = request.body.signature
+    let amount = request.body.amount;
+    let fee = request.body.fee;
+    let sigExpTime = request.body.signature_expiration_time
 
-    const recovered = ethSig.recoverTypedSignature_v4({
-        data: data,
-        sig: signature,
-    });
+    const pk = process.env.PRIVATE_KEY_1;
+    const web3 = new Web3(new Web3.providers.HttpProvider(RPC_URL));
 
-    let verificationStatus = recovered.toLowerCase() === address.toString().toLowerCase()
+    let hash = web3.utils.soliditySha3({t:"uint256", v:amount},{t:"uint256", v:fee},{t:"uint256", v:sigExpTime}, {t: "string", v: "claimHarbr"});
 
     return response.json({
-        "message_signer" : recovered.toLowerCase(),
-        "expected_signer" : address.toLowerCase(),
-        "verification_status": verificationStatus
+        "amount" : amount,
+        "fee" : fee,
+        "sigExpTime": sigExpTime,
+        "signature" : web3.eth.accounts.sign(hash, pk)
     });
 }
 
 exports.sign_conversion_application_to_mint = (request, response) => {
-    const address = request.body.address
-    const data = request.body.data
-    const signature = request.body.signature
+    let amount = request.body.amount;
+    let sigExpTime = request.body.signature_expiration_time
 
-    const recovered = ethSig.recoverTypedSignature_v4({
-        data: data,
-        sig: signature,
-    });
+    const pk = process.env.PRIVATE_KEY_1;
+    const web3 = new Web3(new Web3.providers.HttpProvider(RPC_URL));
 
-    let verificationStatus = recovered.toLowerCase() === address.toString().toLowerCase()
+    let hash = web3.utils.soliditySha3({t:"uint256", v:amount},{t:"uint256", v:sigExpTime}, {t: "string", v: "hundredDayConversionAToH"});
 
     return response.json({
-        "message_signer" : recovered.toLowerCase(),
-        "expected_signer" : address.toLowerCase(),
-        "verification_status": verificationStatus
+        "amount" : amount,
+        "sigExpTime": sigExpTime,
+        "signature" : web3.eth.accounts.sign(hash, pk)
     });
 }
 
 exports.sign_conversion_mint_to_application = (request, response) => {
-    const address = request.body.address
-    const data = request.body.data
-    const signature = request.body.signature
+    let amount = request.body.amount;
+    let sigExpTime = request.body.signature_expiration_time
 
-    const recovered = ethSig.recoverTypedSignature_v4({
-        data: data,
-        sig: signature,
-    });
+    const pk = process.env.PRIVATE_KEY_1;
+    const web3 = new Web3(new Web3.providers.HttpProvider(RPC_URL));
 
-    let verificationStatus = recovered.toLowerCase() === address.toString().toLowerCase()
+    let hash = web3.utils.soliditySha3({t:"uint256", v:amount},{t:"uint256", v:sigExpTime}, {t: "string", v: "hundredDayConversionHToA"});
 
     return response.json({
-        "message_signer" : recovered.toLowerCase(),
-        "expected_signer" : address.toLowerCase(),
-        "verification_status": verificationStatus
+        "amount" : amount,
+        "sigExpTime": sigExpTime,
+        "signature" : web3.eth.accounts.sign(hash, pk)
     });
 }
 
 exports.sign_instant_conversion_application_to_mint = (request, response) => {
-    const address = request.body.address
-    const data = request.body.data
-    const signature = request.body.signature
 
-    const recovered = ethSig.recoverTypedSignature_v4({
-        data: data,
-        sig: signature,
-    });
+    let fromStake = request.body.from_stake;
+    let amount = request.body.amount;
+    let sigExpTime = request.body.signature_expiration_time
 
-    let verificationStatus = recovered.toLowerCase() === address.toString().toLowerCase()
+    const pk = process.env.PRIVATE_KEY_1;
+    const web3 = new Web3(new Web3.providers.HttpProvider(RPC_URL));
+
+    let hash = web3.utils.soliditySha3({t:"bool", v:fromStake},{t:"uint256", v:amount},{t:"uint256", v:sigExpTime}, {t: "string", v: "instantConversionAToH"});
 
     return response.json({
-        "message_signer" : recovered.toLowerCase(),
-        "expected_signer" : address.toLowerCase(),
-        "verification_status": verificationStatus
+        "from_stake" : fromStake,
+        "amount" : amount,
+        "sigExpTime": sigExpTime,
+        "signature" : web3.eth.accounts.sign(hash, pk)
     });
 }
 
 exports.sign_instant_conversion_mint_to_application = (request, response) => {
-    const address = request.body.address
-    const data = request.body.data
-    const signature = request.body.signature
+    let amount = request.body.amount;
+    let sigExpTime = request.body.signature_expiration_time
 
-    const recovered = ethSig.recoverTypedSignature_v4({
-        data: data,
-        sig: signature,
-    });
+    const pk = process.env.PRIVATE_KEY_1;
+    const web3 = new Web3(new Web3.providers.HttpProvider(RPC_URL));
 
-    let verificationStatus = recovered.toLowerCase() === address.toString().toLowerCase()
+    let hash = web3.utils.soliditySha3({t:"uint256", v:amount},{t:"uint256", v:sigExpTime}, {t: "string", v: "instantConversionHToA"});
 
     return response.json({
-        "message_signer" : recovered.toLowerCase(),
-        "expected_signer" : address.toLowerCase(),
-        "verification_status": verificationStatus
+        "amount" : amount,
+        "sigExpTime": sigExpTime,
+        "signature" : web3.eth.accounts.sign(hash, pk)
     });
 }
 
 exports.sign_staking_application_token = (request, response) => {
-    const address = request.body.address
-    const data = request.body.data
-    const signature = request.body.signature
+    let amount = request.body.amount;
+    let sigExpTime = request.body.signature_expiration_time
 
-    const recovered = ethSig.recoverTypedSignature_v4({
-        data: data,
-        sig: signature,
-    });
+    const pk = process.env.PRIVATE_KEY_1;
+    const web3 = new Web3(new Web3.providers.HttpProvider(RPC_URL));
 
-    let verificationStatus = recovered.toLowerCase() === address.toString().toLowerCase()
+    let hash = web3.utils.soliditySha3({t:"uint256", v:amount},{t:"uint256", v:sigExpTime}, {t: "string", v: "stakeArbr"});
 
     return response.json({
-        "message_signer" : recovered.toLowerCase(),
-        "expected_signer" : address.toLowerCase(),
-        "verification_status": verificationStatus
+        "amount" : amount,
+        "sigExpTime": sigExpTime,
+        "signature" : web3.eth.accounts.sign(hash, pk)
     });
 }
 
 exports.sign_withdraw_application_token = (request, response) => {
-    const address = request.body.address
-    const data = request.body.data
-    const signature = request.body.signature
+    let amount = request.body.amount;
+    let sigExpTime = request.body.signature_expiration_time
 
-    const recovered = ethSig.recoverTypedSignature_v4({
-        data: data,
-        sig: signature,
-    });
+    const pk = process.env.PRIVATE_KEY_1;
+    const web3 = new Web3(new Web3.providers.HttpProvider(RPC_URL));
 
-    let verificationStatus = recovered.toLowerCase() === address.toString().toLowerCase()
+    let hash = web3.utils.soliditySha3({t:"uint256", v:amount},{t:"uint256", v:sigExpTime}, {t: "string", v: "withdrawArbr"});
 
     return response.json({
-        "message_signer" : recovered.toLowerCase(),
-        "expected_signer" : address.toLowerCase(),
-        "verification_status": verificationStatus
+        "amount" : amount,
+        "sigExpTime": sigExpTime,
+        "signature" : web3.eth.accounts.sign(hash, pk)
     });
 }
