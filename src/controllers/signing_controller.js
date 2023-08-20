3e5b7ee972754dc339e9dc370bd0a5145d7a97c2
Miljan9602 <rakitamiljan@yahoo.com>
2023-08-20 08:15:05 +0200

    Changed signing methods

--- src/controllers/signing_controller.js
+++ src/controllers/signing_controller.js
@@ -3,26 +3,25 @@ const contractGetters = require("../utils/getters")
 
 const web3 = new Web3(new Web3.providers.HttpProvider(contractGetters.getRpc()));
 
-
 exports.sign_claim_application_token = (request, response) => {
 
     let user = request.body.user;
     let amount = request.body.amount;
     let fee = request.body.fee;
     let sigExpTime = request.body.signature_expiration_time
+    let contractAddress = request.body.contract_address
     let selector = "claimArbr"
 
-    const pk = process.env.PRIVATE_KEY_1;
-
-    let hash = web3.utils.soliditySha3({t:"address", v: user}, {t:"uint256", v:amount},{t:"uint256", v:fee},{t:"uint256", v:sigExpTime}, {t: "string", v: selector});
+    let hash = web3.utils.soliditySha3({t:"address", v: user}, {t:"uint256", v:amount},{t:"uint256", v:fee},{t:"uint256", v:sigExpTime},{t:"address", v: contractAddress}, {t: "string", v: selector});
 
     return response.json({
         "user" : user,
         "amount" : amount,
         "fee" : fee,
-        "sigExpTime": sigExpTime,
+        "sig_exp_time": sigExpTime,
+        "contract_address" : contractAddress,
         "selector" : selector,
-        "signature" : web3.eth.accounts.sign(hash, pk).signature
+        "signature" : web3.eth.accounts.sign(hash, process.env.PRIVATE_KEY_1).signature
     });
 }
 
@@ -32,19 +31,19 @@ exports.sign_claim_mint_token = (request, response) => {
     let amount = request.body.amount;
     let fee = request.body.fee;
     let sigExpTime = request.body.signature_expiration_time
+    let contractAddress = request.body.contract_address
     let selector = "claimHarbr"
 
-    const pk = process.env.PRIVATE_KEY_1;
-
-    let hash = web3.utils.soliditySha3({t:"address", v: user}, {t:"uint256", v:amount},{t:"uint256", v:fee},{t:"uint256", v:sigExpTime}, {t: "string", v: selector});
+    let hash = web3.utils.soliditySha3({t:"address", v: user}, {t:"uint256", v:amount},{t:"uint256", v:fee},{t:"uint256", v:sigExpTime},{t:"address", v: contractAddress}, {t: "string", v: selector});
 
     return response.json({
         "user" : user,
         "amount" : amount,
         "fee" : fee,
-        "sigExpTime": sigExpTime,
+        "sig_exp_time": sigExpTime,
+        "contract_address" : contractAddress,
         "selector" : selector,
-        "signature" : web3.eth.accounts.sign(hash, pk).signature
+        "signature" : web3.eth.accounts.sign(hash, process.env.PRIVATE_KEY_1).signature
     });
 }
 
@@ -52,19 +51,21 @@ exports.sign_conversion_application_to_mint = (request, response) => {
 
     let user = request.body.user;
     let amount = request.body.amount;
+    let fee = request.body.fee;
     let sigExpTime = request.body.signature_expiration_time
+    let contractAddress = request.body.contract_address
     let selector = "hundredDayConversionAToH"
 
-    const pk = process.env.PRIVATE_KEY_1;
-
-    let hash = web3.utils.soliditySha3({t:"address", v: user}, {t:"uint256", v:amount},{t:"uint256", v:sigExpTime}, {t: "string", v: selector});
+    let hash = web3.utils.soliditySha3({t:"address", v: user}, {t:"uint256", v:amount},{t:"uint256", v:fee},{t:"uint256", v:sigExpTime},{t:"address", v: contractAddress}, {t: "string", v: selector});
 
     return response.json({
         "user" : user,
         "amount" : amount,
-        "sigExpTime": sigExpTime,
+        "fee" : fee,
+        "sig_exp_time": sigExpTime,
+        "contract_address" : contractAddress,
         "selector" : selector,
-        "signature" : web3.eth.accounts.sign(hash, pk).signature
+        "signature" : web3.eth.accounts.sign(hash, process.env.PRIVATE_KEY_1).signature
     });
 }
 
@@ -72,41 +73,42 @@ exports.sign_conversion_mint_to_application = (request, response) => {
 
     let user = request.body.user;
     let amount = request.body.amount;
+    let fee = request.body.fee;
     let sigExpTime = request.body.signature_expiration_time
+    let contractAddress = request.body.contract_address
     let selector = "hundredDayConversionHToA"
 
-    const pk = process.env.PRIVATE_KEY_1;
-
-    let hash = web3.utils.soliditySha3({t:"address", v: user}, {t:"uint256", v:amount},{t:"uint256", v:sigExpTime}, {t: "string", v: selector});
+    let hash = web3.utils.soliditySha3({t:"address", v: user}, {t:"uint256", v:amount},{t:"uint256", v:fee},{t:"uint256", v:sigExpTime},{t:"address", v: contractAddress}, {t: "string", v: selector});
 
     return response.json({
         "user" : user,
         "amount" : amount,
-        "sigExpTime": sigExpTime,
+        "fee" : fee,
+        "sig_exp_time": sigExpTime,
+        "contract_address" : contractAddress,
         "selector" : selector,
-        "signature" : web3.eth.accounts.sign(hash, pk).signature
+        "signature" : web3.eth.accounts.sign(hash, process.env.PRIVATE_KEY_1).signature
     });
 }
 
 exports.sign_instant_conversion_application_to_mint = (request, response) => {
-
     let user = request.body.user;
-    let fromStake = request.body.from_stake;
     let amount = request.body.amount;
+    let fee = request.body.fee;
     let sigExpTime = request.body.signature_expiration_time
+    let contractAddress = request.body.contract_address
     let selector = "instantConversionAToH"
 
-    const pk = process.env.PRIVATE_KEY_1;
-
-    let hash = web3.utils.soliditySha3({t:"address", v: user}, {t:"bool", v:fromStake},{t:"uint256", v:amount},{t:"uint256", v:sigExpTime}, {t: "string", v: selector});
+    let hash = web3.utils.soliditySha3({t:"address", v: user}, {t:"uint256", v:amount},{t:"uint256", v:fee},{t:"uint256", v:sigExpTime},{t:"address", v: contractAddress}, {t: "string", v: selector});
 
     return response.json({
         "user" : user,
-        "from_stake" : fromStake,
         "amount" : amount,
-        "sigExpTime": sigExpTime,
+        "fee" : fee,
+        "sig_exp_time": sigExpTime,
+        "contract_address" : contractAddress,
         "selector" : selector,
-        "signature" : web3.eth.accounts.sign(hash, pk).signature
+        "signature" : web3.eth.accounts.sign(hash, process.env.PRIVATE_KEY_1).signature
     });
 }
 
@@ -114,19 +116,21 @@ exports.sign_instant_conversion_mint_to_application = (request, response) => {
 
     let user = request.body.user;
     let amount = request.body.amount;
+    let fee = request.body.fee;
     let sigExpTime = request.body.signature_expiration_time
+    let contractAddress = request.body.contract_address
     let selector = "instantConversionHToA"
 
-    const pk = process.env.PRIVATE_KEY_1;
-
-    let hash = web3.utils.soliditySha3(  {t:"address", v: user},{t:"uint256", v:amount},{t:"uint256", v:sigExpTime}, {t: "string", v: selector});
+    let hash = web3.utils.soliditySha3({t:"address", v: user}, {t:"uint256", v:amount},{t:"uint256", v:fee},{t:"uint256", v:sigExpTime},{t:"address", v: contractAddress}, {t: "string", v: selector});
 
     return response.json({
         "user" : user,
         "amount" : amount,
-        "sigExpTime": sigExpTime,
+        "fee" : fee,
+        "sig_exp_time": sigExpTime,
+        "contract_address" : contractAddress,
         "selector" : selector,
-        "signature" : web3.eth.accounts.sign(hash, pk).signature
+        "signature" : web3.eth.accounts.sign(hash, process.env.PRIVATE_KEY_1).signature
     });
 }
 
@@ -134,19 +138,21 @@ exports.sign_staking_application_token = (request, response) => {
 
     let user = request.body.user;
     let amount = request.body.amount;
+    let fee = request.body.fee;
     let sigExpTime = request.body.signature_expiration_time
+    let contractAddress = request.body.contract_address
     let selector = "stakeArbr"
 
-    const pk = process.env.PRIVATE_KEY_1;
-
-    let hash = web3.utils.soliditySha3({t:"address", v: user},{t:"uint256", v:amount},{t:"uint256", v:sigExpTime}, {t: "string", v: selector});
+    let hash = web3.utils.soliditySha3({t:"address", v: user}, {t:"uint256", v:amount},{t:"uint256", v:fee},{t:"uint256", v:sigExpTime},{t:"address", v: contractAddress}, {t: "string", v: selector});
 
     return response.json({
         "user" : user,
         "amount" : amount,
-        "sigExpTime": sigExpTime,
+        "fee" : fee,
+        "sig_exp_time": sigExpTime,
+        "contract_address" : contractAddress,
         "selector" : selector,
-        "signature" : web3.eth.accounts.sign(hash, pk).signature
+        "signature" : web3.eth.accounts.sign(hash, process.env.PRIVATE_KEY_1).signature
     });
 }
 
@@ -154,18 +160,20 @@ exports.sign_withdraw_application_token = (request, response) => {
 
     let user = request.body.user;
     let amount = request.body.amount;
+    let fee = request.body.fee;
     let sigExpTime = request.body.signature_expiration_time
+    let contractAddress = request.body.contract_address
     let selector = "withdrawArbr"
 
-    const pk = process.env.PRIVATE_KEY_1;
-
-    let hash = web3.utils.soliditySha3({t:"address", v: user},{t:"uint256", v:amount},{t:"uint256", v:sigExpTime}, {t: "string", v: selector});
+    let hash = web3.utils.soliditySha3({t:"address", v: user}, {t:"uint256", v:amount},{t:"uint256", v:fee},{t:"uint256", v:sigExpTime},{t:"address", v: contractAddress}, {t: "string", v: selector});
 
     return response.json({
         "user" : user,
         "amount" : amount,
-        "sigExpTime": sigExpTime,
+        "fee" : fee,
+        "sig_exp_time": sigExpTime,
+        "contract_address" : contractAddress,
         "selector" : selector,
-        "signature" : web3.eth.accounts.sign(hash, pk).signature
+        "signature" : web3.eth.accounts.sign(hash, process.env.PRIVATE_KEY_1).signature
     });
 }
