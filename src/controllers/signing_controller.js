1fcbd3e3f57b737e09e015c7c197685c393b510c
Miljan9602 <rakitamiljan@yahoo.com>
2023-08-21 16:12:58 +0200

    Changed abi and addresses

--- src/controllers/signing_controller.js
+++ src/controllers/signing_controller.js
@@ -101,6 +101,7 @@ exports.sign_instant_conversion_application_to_mint = (request, response) => {
     let contractAddress = request.body.contract_address
     let selector = web3.eth.abi.encodeFunctionSignature("instantConversionAToH(bool,uint256,uint256,bytes[2])");
 
+
     let hash = web3.utils.soliditySha3({t:"address", v: user}, {t:"uint256", v:amount},{t:"uint256", v:fee},{t:"uint256", v:sigExpTime},{t:"address", v: contractAddress}, {t: "bytes4", v: selector});
 
     return response.json({
