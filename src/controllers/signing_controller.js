a6b3c5914f5f1da788b3070031f0c6e688388f39
Miljan9602 <rakitamiljan@yahoo.com>
2023-08-21 14:55:43 +0200

    Redeploy

--- src/controllers/signing_controller.js
+++ src/controllers/signing_controller.js
@@ -93,13 +93,13 @@ exports.sign_conversion_mint_to_application = (request, response) => {
 }
 
 exports.sign_instant_conversion_application_to_mint = (request, response) => {
+
     let user = request.body.user;
     let amount = request.body.amount;
     let fee = request.body.fee;
     let sigExpTime = request.body.signature_expiration_time
     let contractAddress = request.body.contract_address
     let selector = web3.eth.abi.encodeFunctionSignature("instantConversionAToH(bool,uint256,uint256,bytes[2])");
-
 
     let hash = web3.utils.soliditySha3({t:"address", v: user}, {t:"uint256", v:amount},{t:"uint256", v:fee},{t:"uint256", v:sigExpTime},{t:"address", v: contractAddress}, {t: "bytes4", v: selector});
 
