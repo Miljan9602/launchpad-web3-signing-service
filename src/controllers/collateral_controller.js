bd555a8cc99e4ea43cb2670858de31cba4a782cf
Miljan9602 <rakitamiljan@yahoo.com>
2023-09-11 16:16:10 +0200

    Removed non used code

--- src/controllers/collateral_controller.js
+++ src/controllers/collateral_controller.js
@@ -1,4 +1,4 @@
-const { Web3, Contract} = require('web3');
+const { Web3} = require('web3');
 const contractGetters = require("../utils/getters")
 
 const RPC_URL = contractGetters.getRpc();
@@ -32,22 +32,7 @@ exports.sign_deposit = async (request, response) => {
     let contractAddress = request.body.contract_address
     let selector = web3.eth.abi.encodeFunctionSignature("deposit(uint256,uint256,bytes[2])");
 
-    let hash = web3.utils.soliditySha3({t: "address", v: user}, {t: "uint256", v: amount}, {
-        t: "uint256",
-        v: sigExpTime
-    }, {t: "address", v: contractAddress}, {t: "bytes4", v: selector});
-
-    let contract = new Contract(contractGetters.getCollateralAbi(), contractGetters.getCollateralAddress());
-    contract.setProvider(RPC_URL);
-
-    let sig = [web3.eth.accounts.sign(hash, process.env.PRIVATE_KEY_1).signature, web3.eth.accounts.sign(hash, process.env.PRIVATE_KEY_2).signature];
-    let result = await contract.methods.signaturesValidityCheck(web3.eth.accounts.hashMessage(hash), sig).call();
-
-    console.log({
-        "hash": web3.eth.accounts.hashMessage(hash),
-        "SIG": sig,
-        "result": result
-    })
+    let hash = web3.utils.soliditySha3({t: "address", v: user}, {t: "uint256", v: amount}, {t: "uint256", v: sigExpTime}, {t: "address", v: contractAddress}, {t: "bytes4", v: selector});
 
     return response.json({
         "user": user,
