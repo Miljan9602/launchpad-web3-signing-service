173bc9820fe0db34a4f9a16c5008e87d32f8ba19
Miljan9602 <rakitamiljan@yahoo.com>
2023-11-13 17:21:27 +0300

    added is signature used

--- src/controllers/sale_controller.js
+++ src/controllers/sale_controller.js
@@ -1,4 +1,4 @@
-const { Web3} = require('web3');
+const { Web3, Contract} = require('web3');
 const contractGetters = require("../utils/getters")
 
 const RPC_URL = contractGetters.getRpc();
@@ -21,5 +21,25 @@ exports.sign_withdraw_tokens = (request, response) => {
         "contract_address" : contractAddress,
         "selector" : selector,
         "signature" : web3.eth.accounts.sign(hash, process.env.PRIVATE_KEY_1).signature
+    });
+}
+
+exports.is_signature_used = async (request, response) => {
+
+    let user = request.body.user;
+    let amount = request.body.amount;
+    let sigExpTime = request.body.signature_expiration_time
+    let contractAddress = request.body.contract_address
+    let selector = web3.eth.abi.encodeFunctionSignature("withdrawTokens(uint256,uint256,bytes[2])");
+
+    let hash = web3.utils.soliditySha3({t:"address", v: user}, {t:"uint256", v:amount},{t:"uint256", v:sigExpTime},{t:"address", v: contractAddress}, {t: "bytes4", v: selector});
+
+    let contract = new Contract(contractGetters.getStakingAbi(), contractAddress);
+    contract.setProvider(RPC_URL);
+
+    let result = await contract.methods.isMessageUsed(web3.eth.accounts.hashMessage(hash)).call();
+
+    return response.json({
+        "is_used" : result
     });
 }
--- src/routes/sale_routes.js
+++ src/routes/sale_routes.js
@@ -4,6 +4,6 @@ const router = express.Router()
 const sale_controller = require('../controllers/sale_controller');
 
 router.post('/withdraw', sale_controller.sign_withdraw_tokens);
-
+router.post('/is-signature-used', sale_controller.is_signature_used);
 
 module.exports = router
