d1bc75b8178ff46737b05edb5407b680530adeb3
Miljan9602 <rakitamiljan@yahoo.com>
2023-11-15 13:39:26 +0300

    Removed non used endpoint

--- src/controllers/marketplace_controller.js
+++ src/controllers/marketplace_controller.js
@@ -4,27 +4,6 @@ const contractGetters = require("../utils/getters")
 const RPC_URL = contractGetters.getRpc();
 const web3 = new Web3(new Web3.providers.HttpProvider(RPC_URL));
 
-exports.is_signature_used = async (request, response) => {
-
-    let user = request.body.user;
-    let amount = request.body.amount;
-    let fee = request.body.fee;
-    let sigExpTime = request.body.signature_expiration_time
-    let contractAddress = request.body.contract_address
-    let selector = request.body.selector
-
-    let hash = web3.utils.soliditySha3({t:"address", v: user}, {t:"uint256", v:amount},{t:"uint256", v:fee},{t:"uint256", v:sigExpTime},{t:"address", v: contractAddress}, {t: "bytes4", v: selector});
-
-    let contract = new Contract(contractGetters.getStakingAbi(), contractGetters.getStakingAddress());
-    contract.setProvider(RPC_URL);
-
-    let result = await contract.methods.isMessageUsed(web3.eth.accounts.hashMessage(hash)).call();
-
-    return response.json({
-        "is_used" : result
-    });
-}
-
 exports.sign_add_offer = (request, response) => {
 
     let user = request.body.user
