28fd4fe7b69ac9c079fb4ff5c2405cbbf6eb4f6d
Miljan9602 <rakitamiljan@yahoo.com>
2023-11-21 15:58:19 +0100

    Removed update offer signature for signing

--- src/routes/marketplace_routes.js
+++ src/routes/marketplace_routes.js
@@ -5,7 +5,6 @@ const marketplace_controller = require('../controllers/marketplace_controller');
 
 router.post('/add-offer', marketplace_controller.sign_add_offer);
 router.post('/remove-offer', marketplace_controller.sign_remove_offer);
-router.post('/update-offer', marketplace_controller.sign_update_offer);
 router.post('/buy-offer', marketplace_controller.sign_buy_offer);
 
 module.exports = router
