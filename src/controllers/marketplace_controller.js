f76fd43eb30b4369693e4ee2c518a1455e794636
Miljan9602 <rakitamiljan@yahoo.com>
2023-11-22 16:16:49 +0100

    Fixed signatures for buy and remove

--- src/controllers/marketplace_controller.js
+++ src/controllers/marketplace_controller.js
@@ -34,7 +34,7 @@ exports.sign_remove_offer = (request, response) => {
     let selector = web3.eth.abi.encodeFunctionSignature("removeOffer(uint256,uint256,bytes[2])");
     let contractAddress = request.body.contract_address
 
-    let hash = web3.utils.soliditySha3({t:"address", v: user}, {t:"uint256", v:offerId},{t:"uint256", v:sigExpTime},{t:"address", v: contractAddress},{t: "bytes4", v: selector});
+    let hash = web3.utils.soliditySha3({t:"address", v: user}, {t:"uint256", v:0}, {t:"uint256", v:offerId},{t:"uint256", v:sigExpTime},{t:"address", v: contractAddress},{t: "bytes4", v: selector});
 
     return response.json({
         "user" : user,
@@ -53,7 +53,7 @@ exports.sign_buy_offer = (request, response) => {
     let selector = web3.eth.abi.encodeFunctionSignature("buy(uint256,uint256,uint256,bytes[2])");
     let contractAddress = request.body.contract_address
 
-    let hash = web3.utils.soliditySha3({t:"address", v: user}, {t:"uint256", v:offerId},{t:"uint256", v:sigExpTime},{t:"address", v: contractAddress},{t: "bytes4", v: selector});
+    let hash = web3.utils.soliditySha3({t:"address", v: user},{t:"uint256", v:0}, {t:"uint256", v:offerId},{t:"uint256", v:sigExpTime},{t:"address", v: contractAddress},{t: "bytes4", v: selector});
 
     return response.json({
         "user" : user,
